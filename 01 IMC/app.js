f1ec9df550608fa4310c3e44b5ca28df77abdea8
Viridiana Leal <LERV060816MHGLMRA8@soycecytem.mx>
2023-05-09 21:21:44 -0600

    Cuatro

--- 01 IMC/app.js
+++ 01 IMC/app.js
@@ -8,28 +8,24 @@ altura.oninput=()=>{calcular()}
 const calcular=()=>{
     let vpeso=parseInt(peso.value)
     let valtura=parseFloat(altura.value)
-    document.querySelector("#vpeso").innerHTML=vpeso+"KG"
-    document.querySelector("#valtura").innerHTML=valtura+"M"
-
-    let imc=vpeso/(valtura*valtura);
-
-    if(imc<=18.49) {
-        res.innerHTML=imc.toFixed(2);
-        con.innerHTML="BAJO PESO";
-    }else if(imc >=18.50 && imc<=24.99){
-        res.innerHTML=imc.toFixed(2);
-        con.innerHTML="NORMAL";
-    }else if(imc >=25 && imc<=29.99) {
-        res.innerHTML=imc.toFixed(2);
-        con.innerHTML="SOBREPESO";
-    }else if(imc >=30 && imc<=34.99) {
-        res.innerHTML=imc.toFixed(2);
-        con.innerHTML="OBESIDAD 1";
-    }else if(imc >=35 && imc<=39.99) {
-        res.innerHTML=imc.toFixed(2);
-        con.innerHTML="OBESIDAD 2";
-    }else if(imc >=40) {
-        res.innerHTML=imc.toFixed(2);
-        con.innerHTML="OBESIDAD 3";
-    }
+    document.querySelector("#vpeso").innerHTML=vpeso+"Kg"
+    document.querySelector("#valtura").innerHTML=valtura+"m"
+    
+    res.innerHTML="<img src='/img/spinner.gif' width='200px' height='100px'>";
+    setTimeout(()=>{   
+     let imc=vpeso/(valtura*valtura);
+     let con="";
+     if(imc<18.5){ con="BAJ0 PES0" }
+     else if(imc>=18.5&&imc<25){ con="NORMAL" }
+     else if (imc>=25&&imc<30) { con="SOBREPESO" } 
+     else if (imc>=30&&imc<35) { con="OBESiDAD 1"}
+     else if (imc>=35&&imc<40){ con="OBES iDAD 2"  }
+     else{con="OBESIDAD 3"}
+     res.innerHTML=`<div>
+     <h1>IMC</h1>
+     <div><label>${imc.toFixed(2)}</label></div>
+     <h1>CONDICIÓN</h1>
+     <div><label>${con}</label></div>
+     </div>`
+},2000);
 }
